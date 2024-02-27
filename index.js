9430a43f2d54e8b36631b87dc20f87abcbd3c836
RishuPatel <rishuam120@gmail.com>
2024-02-27 13:13:28 +0530

    v1

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const i18n = require('i18n');
+const path = require('path');
+const cors = require('cors')
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+
+i18n.configure({
+    locales: ['en', 'fr'],
+    directory: path.join(__dirname, 'translation'),
+    defaultLocale: 'en',
+    cookie: 'locale',
+  });
+  
+  app.use(i18n.init);
+
+
+  const translateToFrench = (text) => {
+    i18n.setLocale('fr');
+    const translatedText = i18n.__(text);
+    i18n.setLocale('en'); // Reset locale to default
+    return translatedText;
+  };
+  
+  // API Endpoint for translation
+  app.post('/translate', (req, res) => {
+  
+    try {
+        const { text } = req.body;
+    
+        if (!text) {
+          return res.status(400).json({ error: 'Text is required in the request body.' });
+        }
+        
+    
+       
+          i18n.setLocale('fr');
+          const translatedText = i18n.__(text);
+          i18n.setLocale('en'); 
+          res.json({ translatedText });
+       
+    
+        
+      } catch (error) {
+        console.log(error);
+        return res.status(500).json({ error: 'Internal Server Error' });
+      }
+  });
+  
+
+// Start the server
+const port = process.env.PORT || 7000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
