4f52382ee566bec8d0c9140a718a4be1000b7a62
RishuPatel <rishuam120@gmail.com>
2024-02-27 13:45:29 +0530

    v3

--- index.js
+++ index.js
@@ -13,46 +13,51 @@ i18n.configure({
     directory: path.join(__dirname, 'translation'),
     defaultLocale: 'en',
     cookie: 'locale',
-  });
-  
-  app.use(i18n.init);
+});
+
+app.use(i18n.init);
 
 
-  const translateToFrench = (text) => {
+const translateToFrench = (text) => {
     i18n.setLocale('fr');
     const translatedText = i18n.__(text);
     i18n.setLocale('en'); // Reset locale to default
     return translatedText;
-  };
-  
-  // API Endpoint for translation
-  app.post('/translate', (req, res) => {
-  
+};
+
+app.get('/', (req, res) => {
+    res.json({
+        msg: "hii from vercel"
+    })
+})
+// API Endpoint for translation
+app.post('/translate', (req, res) => {
+
     try {
         const { text } = req.body;
-    
+
         if (!text) {
-          return res.status(400).json({ error: 'Text is required in the request body.' });
+            return res.status(400).json({ error: 'Text is required in the request body.' });
         }
-        
-    
-       
-          i18n.setLocale('fr');
-          const translatedText = i18n.__(text);
-          i18n.setLocale('en'); 
-          res.json({ translatedText });
-       
-    
-        
-      } catch (error) {
+
+
+
+        i18n.setLocale('fr');
+        const translatedText = i18n.__(text);
+        i18n.setLocale('en');
+        res.json({ translatedText });
+
+
+
+    } catch (error) {
         console.log(error);
         return res.status(500).json({ error: 'Internal Server Error' });
-      }
-  });
-  
+    }
+});
+
 
 // Start the server
 const port = process.env.PORT || 7000;
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
